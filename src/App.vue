60a3ada5d7fe4e9d4de893c69f9d2e049639e92f
LiJun <lijunal@126.com>
2023-03-29 14:03:02 +0800

    基础配置

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Table from "@/components/table/index.vue";
+const tableData = ref([
+	{
+		date: "2016-05-03",
+		name: "Tom",
+		address: "No. 189, Grove St, Los Angeles",
+	},
+	{
+		date: "2016-05-02",
+		name: "Tom",
+		address: "No. 189, Grove St, Los Angeles",
+	},
+	{
+		date: "2016-05-04",
+		name: "Tom",
+		address: "No. 189, Grove St, Los Angeles",
+	},
+	{
+		date: "2016-05-01",
+		name: "Tom",
+		address: "No. 189, Grove St, Los Angeles",
+	},
+]);
+</script>
+
+<template>
+	<div class="tac mt10">
+		<a class="btn">导出</a>
+	</div>
+	<div class="w90per mauto">
+		<Table :data="tableData" />
+	</div>
+</template>
+
+<style lang="less" scoped>
+.btn {
+	display: inline-block;
+	padding: 6px 20px;
+	color: #fff;
+	background: #2065fc;
+	border-radius: 4px;
+	margin-bottom: 10px;
+	font-size: 14px;
+	cursor: pointer;
+	&:hover {
+		opacity: 0.8;
+	}
+}
+</style>
---
+++ src/components/table/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+	data?: any;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+	data: () => [],
+});
+</script>
+
+<template>
+	<el-table :data="props.data" border stripe>
+		<el-table-column prop="date" label="Date" width="180" />
+		<el-table-column prop="name" label="Name" width="180" />
+		<el-table-column prop="address" label="Address" />
+	</el-table>
+</template>
+
+<style lang="less" scoped></style>
